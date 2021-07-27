99289aefa01297d6687b1982b91951ddd01b7173
mrabrahams <62486253+mrabrahams@users.noreply.github.com>
2021-07-27 14:50:38 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,62 @@
+# learning and implementing binary search trees
+
+# naive search algorithm searches every index in list until target is found
+import random
+import time
+
+
+def naive_search(l, target):
+    # example l = [1, 3, 10, 12]
+    for i in range(len(l)):
+        if l[i] == target:
+            return i
+    return -1
+
+
+# splits list in half and searches the appropriate section depending on if target value is in that section
+# list needs to be sorted.
+def binary_search(l, target, low=None, high=None):
+    if low is None:
+        low = 0
+    if high is None:
+        high = len(l) - 1
+
+    if high < low:
+        return -1
+
+    # example l = [1, 3, 5, 10, 12]  # should return 3
+    midpoint = (low + high) // 2  # 2
+
+    if l[midpoint] == target:
+        return midpoint
+    elif target < l[midpoint]:
+        return binary_search(l, target, low, midpoint - 1)
+    else:
+        # target > l[midpoint]
+        return binary_search(l, target, midpoint + 1, high)
+
+
+if __name__ == '__main__':
+    # l = [1, 3, 5, 10, 12]
+    # target = 12
+    # print(naive_search(l, target))
+    # print(binary_search(l, target))
+
+    length = 10000
+    # build a sorted list of length 10000
+    sorted_list = set()
+    while len(sorted_list) < length:
+        sorted_list.add(random.randint(-3 * length, 3 * length))
+    sorted_list = sorted(list(sorted_list))
+
+    start = time.time()
+    for target in sorted_list:
+        naive_search(sorted_list, target)
+    end = time.time()
+    print("Naive search time: ", (end - start) / length, " seconds.")
+
+    start = time.time()
+    for target in sorted_list:
+        binary_search(sorted_list, target)
+    end = time.time()
+    print("Binary search time: ", (end - start) / length, " seconds.")
